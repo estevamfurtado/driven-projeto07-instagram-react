d33920a66318445d196236feca926aed35455953
pedro imbrech <elprogramadorpedro@gmail.com>
2022-03-18 18:04:46 -0300

    rev

--- src/components/Navbar/IconesMobile.js
+++
@@ -1,9 +0,0 @@
-
-
-export default function IconesMobile() {
-    return (
-        <div class="icones-mobile">
-                <ion-icon name="paper-plane-outline"></ion-icon>
-            </div>
-    );
-}
